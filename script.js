1321a99d084c65f4a08b23006da2e3ca713c3871
Shushant Giri <shushantgiri98@gmail.com>
2026-07-16 07:39:02 +0545

    Test_Expense_Tracker_LOCUS

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('form');
+const amount = document.querySelector('amount');
+const category = document.querySelector('category');
+const description = document.querySelector('decription')
+
+
+const tbody = document.querySelector('tbody')
+form,addEventListener("submit", (e)=>)
